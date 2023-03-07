6204510d62d75f0424c8ca00d36ad9e706ec9c59
Thera Avar <123700090+theraavar@users.noreply.github.com>
2023-03-07 15:54:53 -0600

    Update index.js
    Wrote a very questionable query and if -> return for playerChoice

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function getPlayerChoice(){
-  
+  var choice = document.querySelectorAll("input[type="image"]:click");
+  if(choice.name === "rock"){
+    return 0;
+  }else if(choice.name === "paper"){
+    return 1;
+  }else if(choice.name === "scissors"){
+    return 2;
+  }else{
+    return undefined;
+  }                                  
 }
 function getComputerChoice(){
    
