646c85409acd3048da54518b6e1279e0aa9f28c0
theraavar <theraavar@proton.me>
2023-05-05 14:03:48 -0500

    minor ocd organization change

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ function playRound(playerSelection, computerSelection){
         case "rock":
             switch(computerSelection){
                 case "rock": return "It's a tie!";
-                case "scissors": return "You win! Rock smashes scissors!";
                 case "paper": return "You lose! Paper covers rock!";
-            }
-        case "scissors":
-            switch(computerSelection){
-                case "rock": return "You lose! Rock smashes scissors!";
-                case "scissors": return "It's a tie!";
-                case "paper": return "You win! Scissors cuts paper!";
+                case "scissors": return "You win! Rock smashes scissors!";
             }
         case "paper":
             switch(computerSelection){
                 case "rock": return "You win! Paper covers rock!";
-                case "scissors": return "You lose! Scissors cuts paper!";
                 case "paper": return "It's a tie!";
+                case "scissors": return "You lose! Scissors cuts paper!";
+            }
+        case "scissors":
+            switch(computerSelection){
+                case "rock": return "You lose! Rock smashes scissors!";
+                case "paper": return "You win! Scissors cuts paper!";
+                case "scissors": return "It's a tie!";
             }
     }
 }
