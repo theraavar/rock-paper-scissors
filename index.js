27b37a121623e2cee526977e63a8a3f77e365c44
theraavar <theraavar@proton.me>
2023-05-05 13:14:54 -0500

    Added getcomputerchoice method to return random value 0,1,2 and playRound function to compare player and computer selection, returning who wins.

---
+++ index.js
@@ -0,0 +1,34 @@
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3);
+    switch(choice){
+        case 0: return "rock";
+        case 1: return "scissors";
+        case 2: return "paper";
+    }
+}
+function playRound(playerSelection, computerSelection){
+    console.log(playerSelection);
+    console.log(computerSelection);
+    playerSelection = playerSelection.toLowerCase(); //Can't never trust a user to type correctly around here.
+    switch(playerSelection){
+        case "rock":
+            switch(computerSelection){
+                case "rock": return "It's a tie!";
+                case "scissors": return "You win! Rock smashes scissors!";
+                case "paper": return "You lose! Paper covers rock!";
+            }
+        case "scissors":
+            switch(computerSelection){
+                case "rock": return "You lose! Rock smashes scissors!";
+                case "scissors": return "It's a tie!";
+                case "paper": return "You win! Scissors cuts paper!";
+            }
+        case "paper":
+            switch(computerSelection){
+                case "rock": return "You win! Paper covers rock!";
+                case "scissors": return "You lose! Scissors cuts paper!";
+                case "paper": return "It's a tie!";
+            }
+    }
+}
+console.log(playRound("rock", getComputerChoice()));
