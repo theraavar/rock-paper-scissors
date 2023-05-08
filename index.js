c46bbc9a53208bbde10e683f957ba0c4ad117521
theraavar <theraavar@proton.me>
2023-05-08 15:25:21 -0500

    Rewriting the code to work with the ui

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function getComputerChoice(){
         case 2: return "paper";
     }
 }
+/*
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase(); //Can't never trust a user to type correctly around here.
     switch(playerSelection){
         case "rock":
             switch(computerSelection){
@@ -28,7 +28,8 @@ function playRound(playerSelection, computerSelection){
                 case "scissors": return "It's a tie!";
             }
     }
-}
+} 
+*/
 function playGame(rounds){
     let playerScore = 0;
     let computerScore = 0;
@@ -53,3 +54,90 @@ function playGame(rounds){
     }
 }
 console.log(playGame(5));
+
+/*
+For reference, choices are desginated as such:
+    0 = rock
+    1 = paper
+    2 = scissors
+*/
+var playerScore;
+var computerScore;
+var rounds;
+var currentRound;
+
+const getComputerChoice = function() {
+    return Math.floor(Math.random() * 3);
+}
+const playRound = function(playerChoice, computerChoice = getComputerChoice()) {
+    switch(playerChoice){
+        case 0:
+            switch(computerChoice){
+                case 0: 
+                    return;
+                case 1: 
+                    this.computerScore++; return;
+                case 2:
+                    this.playerScore++; return;
+            }
+        case 1:
+            switch(computerChoice){
+                case 0:
+                    this.playerScore++; return;
+                case 1:
+                    return;
+                case 2:
+                    this.computerScore++; return;
+            }
+        case 2:
+            switch(computerChoice){
+                case 0:
+                    this.computerScore++; return;
+                case 1:
+                    this.playerScore++; return;
+                case 2:
+                    return;
+            }
+    }
+    /*
+    At the end of the round, we check to see if there are any rounds left, and if there are, we display the choices again. 
+    */
+   if(this.currentRound < this.rounds){
+    displayChoices;
+   }else{
+    endGame;
+   }
+}
+const playGame = function(rounds) {
+    this.rounds = rounds;
+    /*
+    Once we determine how many rounds we play, we will hide the selection for rounds.
+    */
+}
+const displaySelector = function () {
+
+}
+const displayChoices = function() {
+
+}
+const displayWinner = function() {
+
+}
+const endGame = function() {
+    if(this.playerScore > this.computerChoice){
+        //WIN
+    }else if(this.playerScore < this.computerChoice){
+        //LOSE
+    }else{
+        //TIE
+    }
+    init;
+    displaySelector;
+}
+const init = function(){
+    this.playerScore = 0;
+    this.computerScore = 0;
+    this.rounds = 0;
+    this.currentRound = 0;
+}
+init;
