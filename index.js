c987d609955bd5b4804467c63978c329b2452442
theraavar <theraavar@proton.me>
2023-05-08 15:39:29 -0500

    Testing element selection and event listening; removed old code.

--- index.js
+++ index.js
@@ -1,60 +1,3 @@
-function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3);
-    switch(choice){
-        case 0: return "rock";
-        case 1: return "scissors";
-        case 2: return "paper";
-    }
-}
-/*
-function playRound(playerSelection, computerSelection){
-    switch(playerSelection){
-        case "rock":
-            switch(computerSelection){
-                case "rock": return "It's a tie!";
-                case "paper": return "You lose! Paper covers rock!";
-                case "scissors": return "You win! Rock smashes scissors!";
-            }
-        case "paper":
-            switch(computerSelection){
-                case "rock": return "You win! Paper covers rock!";
-                case "paper": return "It's a tie!";
-                case "scissors": return "You lose! Scissors cuts paper!";
-            }
-        case "scissors":
-            switch(computerSelection){
-                case "rock": return "You lose! Rock smashes scissors!";
-                case "paper": return "You win! Scissors cuts paper!";
-                case "scissors": return "It's a tie!";
-            }
-    }
-} 
-*/
-function playGame(rounds){
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < rounds; i++){
-        let playerSelection = "rock"; //prompt("Please type one of the following: rock | paper | scissors");
-        let result = playRound(playerSelection, getComputerChoice());
-        console.log(result);
-        if(result.includes("win") === true){
-            playerScore++;
-        }else if(result.includes("lose") === true){
-            computerScore++;
-        }
-        console.log(playerScore);
-        console.log(computerScore);
-    }
-    if(playerScore > computerScore){
-        return "You won the game! Congratulations!";
-    }else if(playerScore < computerScore){
-        return "The computer beat you this time!";
-    }else{
-        return "It is a tie! So close!";
-    }
-}
-console.log(playGame(5));
-
 /*
 For reference, choices are desginated as such:
     0 = rock
@@ -140,5 +83,14 @@ const init = function(){
     this.computerScore = 0;
     this.rounds = 0;
     this.currentRound = 0;
+
+    let selectRock = document.getElementById("rock");
+    let selectPaper = document.getElementById("paper");
+    let selectScissors = document.getElementById("paper");
+
+    let select = [selectRock, selectPaper, selectScissors];
+    for(let i = 0; i < select.length; i++){
+        console.log(select[i]);
+    }
 }
 init;
