553545f6527876eb1edad864f7225310d2d14e51
theraavar <theraavar@proton.me>
2023-05-10 12:51:10 -0500

    fixed the visibility toggle, added documentation.

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+/* These are variables related to our DOM. */
+var eRounds = document.querySelector(".container-rounds");
+var eChoices = document.querySelector(".container-choices");
+var eWinner = document.querySelector(".container-winner");
+
+/* This is a simple toggle for whether the element is displayed or not. */
+function toggleVisibility(e){
+    if(e.style.display === "none"){
+        e.style.display = "block";
+    }else{
+        e.style.display = "none";
+    }
+}
+
+/* We hide the following sections by default on load. */
+toggleVisibility(eChoices);
+toggleVisibility(eWinner);
+
+/* These are variables related to the game. */
 var playerScore;
 var computerScore;
 
 var currentRound;
 var totalRounds;
 
+/* This is called when the user selects how many rounds they want to play. */
 function playGame(numberOfRounds){
     this.totalRounds = numberOfRounds;
 }
+/* This is called when the user selects one of the choices. */
 function playRound(playerSelection, computerSelection = getComputerSelection()){
     if(playerSelection === computerSelection){
         return "It is a tie!";
@@ -32,9 +53,11 @@ function playRound(playerSelection, computerSelection = getComputerSelection()){
             }
     }
 }
+/* This is a simple random return of 0, 1 or 2. */
 function getComputerSelection(){
     return Math.floor(Math.random() * 3);
 }
+/* We call this after everything else is loaded so our variables have a base value. We also use this to reset the game after. */
 function init(){
     this.playerScore = 0;
     this.computerScore = 0;
